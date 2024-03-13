41800ee6fd3ae725215914b20fa65c6e1cac7449
niyomozey <moiseniyonkuru1@gmail.com>
2024-03-13 23:57:12 +0200

    add set polygon

--- app/src/main/java/org/technoserve/farmcollector/ContextExt.kt
+++ app/src/main/java/org/technoserve/farmcollector/ContextExt.kt
@@ -5,13 +5,10 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 
-fun Context.hasLocationPermission():Boolean{
+fun Context.hasLocationPermission(): Boolean {
     return ContextCompat.checkSelfPermission(
-        this,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    ) == PackageManager.PERMISSION_GRANTED &&
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+        this, Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+        this, Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
 }
--- app/src/main/java/org/technoserve/farmcollector/ui/screens/SetPolygon.kt
+++ app/src/main/java/org/technoserve/farmcollector/ui/screens/SetPolygon.kt
@@ -1,9 +1,15 @@
 package org.technoserve.farmcollector.ui.screens
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Looper
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -12,67 +18,112 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import org.technoserve.farmcollector.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.core.app.ActivityCompat
+import org.technoserve.farmcollector.hasLocationPermission
 
 @Composable
-fun SetPolygon(navController: NavController)
-{
-    // Screen for displaying and setting farm polygon coordinates
+fun SetPolygon(navController: NavController) {
+    val context = LocalContext.current as Activity
+    var coordinates by remember { mutableStateOf(listOf<String>()) }
+    var isCapturingCoordinates by remember { mutableStateOf(false) }
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()// Occupy the entire width of the screen
-                .fillMaxHeight(0.7f)// Half of the screen height
+                .fillMaxWidth()
+                .fillMaxHeight(0.7f)
                 .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Oops! You are Offine!!")
+            Text(text = "Oops! You are Offline!!")
         }
+
         Column(
-            modifier = Modifier.background(Color.DarkGray)
+            modifier = Modifier
+                .background(Color.DarkGray)
                 .padding(16.dp)
                 .fillMaxWidth()
-                .fillMaxHeight(),
-
+                .fillMaxHeight()
         ) {
             Text(
                 text = "Coordinates of the farm polygon",
                 fontSize = 20.sp
             )
             Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "[90.80], [909,989], [9090,9.343], [4343.102.45]")
+            Text(text = coordinates.joinToString(separator = ", "))
+
             Row(
                 modifier = Modifier.fillMaxWidth(0.6f),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Start")
+                Button(
+                    onClick = {
+                        isCapturingCoordinates = !isCapturingCoordinates
+                        if (isCapturingCoordinates) {
+                            coordinates// Clear coordinates array when starting
+                        }
+                    }
+                ) {
+                    Text(text = if (isCapturingCoordinates) "Close" else "Start")
                 }
-                Spacer(modifier = Modifier.width(16.dp)) // Add space between the latitude and longitude input fields
-                Button(onClick = { /*TODO*/ }) {
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Button(
+                    onClick = {
+                        if (context.hasLocationPermission() && isCapturingCoordinates) {
+                            val locationRequest = LocationRequest.create().apply {
+                                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//                                interval = 10000 // Update interval in milliseconds
+//                                fastestInterval = 5000 // Fastest update interval in milliseconds
+                            }
+                            fusedLocationClient.requestLocationUpdates(
+                                locationRequest,
+                                object : LocationCallback() {
+                                    @SuppressLint("MissingPermission")
+                                    override fun onLocationResult(locationResult: LocationResult) {
+                                        locationResult.lastLocation?.let { lastLocation ->
+                                            coordinates =
+                                                coordinates + "[${lastLocation.latitude}, ${lastLocation.longitude}]"
+                                        }
+                                    }
+                                },
+                                Looper.getMainLooper()
+                            )
+                        }
+                    }
+                ) {
                     Text(text = "Add Point")
                 }
             }
         }
     }
-
-
 }
+
