470c71a4d87aa4d1cc6c9e3dba5b5d32fc9135d4
lucienshema <lshema@tns.org>
2024-07-09 12:59:35 +0200

    fixed sqm and added unit in update form

--- app/src/main/java/org/technoserve/farmcollector/ui/screens/FarmList.kt
+++ app/src/main/java/org/technoserve/farmcollector/ui/screens/FarmList.kt
@@ -38,8 +38,11 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -47,6 +50,7 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -638,6 +642,9 @@ fun UpdateFarmForm(navController: NavController, farmId: Long?, listItems: List<
         Objects.requireNonNull(context),
         context.packageName + ".provider", file
     )
+    var expanded by remember { mutableStateOf(false) }
+    val items = listOf("Ha", "Acres", "Sqm", "Timad", "Fichesa", "Manzana", "Tarea")
+    var selectedUnit by remember { mutableStateOf(items[0]) }
 
 
     LaunchedEffect(Unit) {
@@ -745,7 +752,8 @@ fun UpdateFarmForm(navController: NavController, farmId: Long?, listItems: List<
 //            }
 
             // Ensure the coordinates have at least 4 points for size greater than 4 hectares
-            if ((size.toFloatOrNull() ?: 0f) >= 4) {
+//            if ((size.toFloatOrNull() ?: 0f) >= 4) {
+            if ((size.toDoubleOrNull()?.let { convertSize(it, selectedUnit).toFloat() } ?: 0f) >= 4) {
                 if ((coordinates?.size ?: 0) < 3) {
                     Toast.makeText(context, "Please capture at least 4 points for the polygon when the size is greater than 4 hectares.", Toast.LENGTH_SHORT).show()
                     return
@@ -755,7 +763,8 @@ fun UpdateFarmForm(navController: NavController, farmId: Long?, listItems: List<
                 item.coordinates = coordinates ?: emptyList()
             }
 
-            item.size = size.toFloat()
+//            item.size = size.toFloat()
+            item.size= convertSize(size.toDouble(), selectedUnit).toFloat()
             item.purchases = 0.toFloat()
             item.updatedAt = Instant.now().millis
             updateFarm(farmViewModel, item)
@@ -900,44 +909,114 @@ fun UpdateFarmForm(navController: NavController, farmId: Long?, listItems: List<
                 .padding(bottom = 16.dp)
         )
 
-        TextField(
-            singleLine = true,
-            value = size,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-            ),
-            onValueChange = { size = it },
-            label = { Text(stringResource(id = R.string.size_in_hectares)) },
-            isError = size.toFloatOrNull() == null || size.toFloat() <= 0, // Validate size
-            modifier = Modifier
-                .focusRequester(focusRequester3)
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
-        Spacer(modifier = Modifier.height(16.dp)) // Add space between the latitude and longitude input fields
+//        TextField(
+//            singleLine = true,
+//            value = size,
+//            keyboardOptions = KeyboardOptions.Default.copy(
+//                keyboardType = KeyboardType.Number,
+//            ),
+//            onValueChange = { size = it },
+//            label = { Text(stringResource(id = R.string.size_in_hectares)) },
+//            isError = size.toFloatOrNull() == null || size.toFloat() <= 0, // Validate size
+//            modifier = Modifier
+//                .focusRequester(focusRequester3)
+//                .fillMaxWidth()
+//                .padding(bottom = 16.dp)
+//        )
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             TextField(
-                readOnly = true,
-                value = latitude,
-                onValueChange = { latitude = it },
-                label = { Text(stringResource(id = R.string.latitude)) },
+                singleLine = true,
+                value = size,
+                onValueChange = {
+                    size = it
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number,
+                ),
+
+                label = { Text(stringResource(id = R.string.size_in_hectares) + " (*)") },
+                isError = size.toFloatOrNull() == null || size.toFloat() <= 0, // Validate size
+                colors = TextFieldDefaults.textFieldColors(
+                    errorLeadingIconColor = Color.Red,
+                ),
                 modifier = Modifier
+                    .focusRequester(focusRequester3)
                     .weight(1f)
                     .padding(bottom = 16.dp)
             )
-            Spacer(modifier = Modifier.width(16.dp)) // Add space between the latitude and longitude input fields
-            TextField(
-                readOnly = true,
-                value = longitude,
-                onValueChange = { longitude = it },
-                label = { Text(stringResource(id = R.string.longitude)) },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = 16.dp)
-            )
+
+            //AreaInputField( farmViewModel = farmViewModel)
+            Spacer(modifier = Modifier.width(16.dp))
+            // Size measure
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = {
+                    expanded = !expanded
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                TextField(
+                    readOnly = true,
+                    value = selectedUnit,
+                    onValueChange = { },
+                    label = { Text(stringResource(R.string.unit)) },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expanded
+                        )
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = {
+                        expanded = false
+                    }
+                ) {
+                    items.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            { Text(text = selectionOption) },
+                            onClick = {
+                                selectedUnit = selectionOption
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp)) // Add space between the latitude and longitude input fields
+        if ((size.toDoubleOrNull()?.let { convertSize(it, selectedUnit).toFloat() } ?: 0f) < 4f) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                TextField(
+                    readOnly = true,
+                    value = latitude,
+                    onValueChange = { latitude = it },
+                    label = { Text(stringResource(id = R.string.latitude)) },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(bottom = 16.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp)) // Add space between the latitude and longitude input fields
+                TextField(
+                    readOnly = true,
+                    value = longitude,
+                    onValueChange = { longitude = it },
+                    label = { Text(stringResource(id = R.string.longitude)) },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(bottom = 16.dp)
+                )
+            }
         }
         Button(
             onClick = {
@@ -946,7 +1025,8 @@ fun UpdateFarmForm(navController: NavController, farmId: Long?, listItems: List<
                     showLocationDialog.value = true
                 } else {
                     if (isLocationEnabled(context) && context.hasLocationPermission()) {
-                        if (size.toFloatOrNull() != null && size.toFloat() < 4) {
+//                        if (size.toFloatOrNull() != null && size.toFloat() < 4) {
+                        if (size.toDoubleOrNull()?.let { convertSize(it, selectedUnit).toFloat() }?.let { it < 4f } == true) {
                         // Simulate collecting latitude and longitude
                             if (context.hasLocationPermission()) {
                                 val locationRequest = LocationRequest.create().apply {
@@ -991,7 +1071,8 @@ fun UpdateFarmForm(navController: NavController, farmId: Long?, listItems: List<
             enabled = size.toFloatOrNull() != null
         ) {
             Text(
-                text = if (size.toFloatOrNull() != null && size.toFloat() < 4) stringResource(id = R.string.get_coordinates) else stringResource(
+                //text = if (size.toFloatOrNull() != null && size.toFloat() < 4) stringResource(id = R.string.get_coordinates) else stringResource(
+                text = if (size.toDoubleOrNull()?.let { convertSize(it, selectedUnit).toFloat() }?.let { it < 4f } == true) stringResource(id = R.string.get_coordinates) else stringResource(
                     id = R.string.set_new_polygon
                 )
             )
--- app/src/main/java/org/technoserve/farmcollector/utils/farmSizeConverter.kt
+++ app/src/main/java/org/technoserve/farmcollector/utils/farmSizeConverter.kt
@@ -4,7 +4,7 @@ fun convertSize(size: Double, selectedUnit: String): Double {
     return when (selectedUnit) {
         "Ha" -> size // If already in hectares, return as is
         "Acres" -> size * 0.404686 // Convert Acres to hectares
-        "sqm" -> size * 0.0001 // Convert square meters to hectares
+        "Sqm" -> size * 0.0001 // Convert square meters to hectares
         "Timad" -> size * 0.24
         "Fichesa" -> size * 0.25
         "Manzana" -> (size * 0.0001) * 7000
