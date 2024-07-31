07726a0490a2df62240f8f771ce28594a6a3f731
lucienshema <lshema@tns.org>
2024-07-31 10:25:00 +0200

    fixed migration from version 2.15 to version version 2.19.3

--- app/src/main/java/org/technoserve/farmcollector/database/AppDatabase.kt
+++ app/src/main/java/org/technoserve/farmcollector/database/AppDatabase.kt
@@ -62,58 +62,6 @@ abstract class AppDatabase : RoomDatabase() {
                 memberId         TEXT    NOT NULL,
                 village          TEXT    NOT NULL,
                 district         TEXT    NOT NULL,
-                purchases        REAL    NOT NULL,
-                size             REAL    NOT NULL,
-                latitude         TEXT    NOT NULL,
-                longitude        TEXT    NOT NULL,
-                coordinates      TEXT,
-                synced           INTEGER NOT NULL DEFAULT 0,
-                scheduledForSync INTEGER NOT NULL DEFAULT 0,
-                createdAt        INTEGER NOT NULL,
-                updatedAt        INTEGER NOT NULL,
-                needsUpdate      INTEGER NOT NULL DEFAULT 0,
-                id               INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                FOREIGN KEY (siteId)
-                REFERENCES CollectionSites (siteId) ON UPDATE NO ACTION
-                                                    ON DELETE CASCADE
-            )
-        """.trimIndent())
-
-                // Step 2: Copy data from the old table to the new table, setting needsUpdate to 0
-                db.execSQL("""
-            INSERT INTO new_Farms (
-                siteId, remote_id, farmerPhoto, farmerName, memberId,
-                village, district, purchases, size, latitude, longitude,
-                coordinates, synced, scheduledForSync, createdAt, updatedAt, needsUpdate, id
-            )
-            SELECT
-                siteId, remote_id, farmerPhoto, farmerName, memberId,
-                village, district, purchases, size, latitude, longitude,
-                coordinates, synced, scheduledForSync, createdAt, updatedAt, 0 AS needsUpdate, id
-            FROM Farms
-        """.trimIndent())
-
-                // Step 3: Drop the old table
-                db.execSQL("DROP TABLE Farms")
-
-                // Step 4: Rename the new table to the original table name
-                db.execSQL("ALTER TABLE new_Farms RENAME TO Farms")
-            }
-        }
-
-        // Define a migration from version 13 to 16
-        private val MIGRATION_13_16 = object : Migration(13, 16) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                // Step 1: Create a new temporary table with the updated schema
-                db.execSQL("""
-            CREATE TABLE new_Farms (
-                siteId           INTEGER NOT NULL,
-                remote_id        BLOB    NOT NULL,
-                farmerPhoto      TEXT    NOT NULL,
-                farmerName       TEXT    NOT NULL,
-                memberId         TEXT    NOT NULL,
-                village          TEXT    NOT NULL,
-                district         TEXT    NOT NULL,
                 purchases        REAL,
                 size             REAL    NOT NULL,
                 latitude         TEXT    NOT NULL,
@@ -152,6 +100,58 @@ abstract class AppDatabase : RoomDatabase() {
                 db.execSQL("ALTER TABLE new_Farms RENAME TO Farms")
             }
         }
+
+//        // Define a migration from version 13 to 16
+//        private val MIGRATION_13_16 = object : Migration(13, 16) {
+//            override fun migrate(db: SupportSQLiteDatabase) {
+//                // Step 1: Create a new temporary table with the updated schema
+//                db.execSQL("""
+//            CREATE TABLE new_Farms (
+//                siteId           INTEGER NOT NULL,
+//                remote_id        BLOB    NOT NULL,
+//                farmerPhoto      TEXT    NOT NULL,
+//                farmerName       TEXT    NOT NULL,
+//                memberId         TEXT    NOT NULL,
+//                village          TEXT    NOT NULL,
+//                district         TEXT    NOT NULL,
+//                purchases        REAL,
+//                size             REAL    NOT NULL,
+//                latitude         TEXT    NOT NULL,
+//                longitude        TEXT    NOT NULL,
+//                coordinates      TEXT,
+//                synced           INTEGER NOT NULL DEFAULT 0,
+//                scheduledForSync INTEGER NOT NULL DEFAULT 0,
+//                createdAt        INTEGER NOT NULL,
+//                updatedAt        INTEGER NOT NULL,
+//                needsUpdate      INTEGER NOT NULL DEFAULT 0,
+//                id               INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+//                FOREIGN KEY (siteId)
+//                REFERENCES CollectionSites (siteId) ON UPDATE NO ACTION
+//                                                    ON DELETE CASCADE
+//            )
+//        """.trimIndent())
+//
+//                // Step 2: Copy data from the old table to the new table, setting needsUpdate to 0
+//                db.execSQL("""
+//            INSERT INTO new_Farms (
+//                siteId, remote_id, farmerPhoto, farmerName, memberId,
+//                village, district, purchases, size, latitude, longitude,
+//                coordinates, synced, scheduledForSync, createdAt, updatedAt, needsUpdate, id
+//            )
+//            SELECT
+//                siteId, remote_id, farmerPhoto, farmerName, memberId,
+//                village, district, purchases, size, latitude, longitude,
+//                coordinates, synced, scheduledForSync, createdAt, updatedAt, 0 AS needsUpdate, id
+//            FROM Farms
+//        """.trimIndent())
+//
+//                // Step 3: Drop the old table
+//                db.execSQL("DROP TABLE Farms")
+//
+//                // Step 4: Rename the new table to the original table name
+//                db.execSQL("ALTER TABLE new_Farms RENAME TO Farms")
+//            }
+//        }
 
         // Define a migration from version 15 to 16
         private val MIGRATION_15_16 = object : Migration(15, 16) {
@@ -193,7 +193,7 @@ abstract class AppDatabase : RoomDatabase() {
             SELECT
                 siteId, remote_id, farmerPhoto, farmerName, memberId,
                 village, district, purchases, size, latitude, longitude,
-                coordinates, synced, scheduledForSync, createdAt, updatedAt,needsUpdate, id
+                coordinates, synced, scheduledForSync, createdAt, updatedAt,0 AS needsUpdate, id
             FROM Farms
         """.trimIndent())
 
